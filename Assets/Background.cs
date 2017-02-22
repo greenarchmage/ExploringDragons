731fa95a90ffcee0050f83806ce47a6e99b15657
Theorclord <mikkeltheorclord@hotmail.com>
2017-02-22 17:03:16 +0100

    initial scene

---
+++ Assets/Background.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Background : MonoBehaviour {
+  
+	// Use this for initialization
+	void Start () {
+    // scales the background to the size of the game
+    float vertExtent = Camera.main.orthographicSize;
+    float horzExtent = vertExtent * Screen.width / Screen.height;
+
+    transform.localScale = new Vector3(horzExtent*2, vertExtent*2);
+  }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
